a8c7c2c5085516d45bc6de671fd2861142318560
OLIA2041052 <2041052@cstj.qc.ca>
2024-03-20 17:40:22 -0400

    Feature_images-codes

--- src/assets/index.js
+++ src/assets/index.js
@@ -10,6 +10,26 @@ import imgPhoto from './img/photo.png'
 
 import testSVG from './img/svgTest.svg'
 
+import C1Heures1 from './img/HORLOGE_0405.svg'
+import C1Heures2 from './img/HORLOGE_1125.svg'
+import C1Heures3 from './img/HORLOGE_1159.svg'
+
+import C2Menace1 from './img/MENACE_Canard.svg'
+import C2Menace2 from './img/MENACE_Cochon.svg'
+import C2Menace3 from './img/MENACE_Vache.svg'
+
+import C3Code1 from './img/CODE4592_Bon.svg'
+import C3Code2 from './img/CODE4592_mauvais1.svg'
+import C3Code3 from './img/CODE4592_mauvais2.svg'
+
+import C4Corbeille1 from './img/MESSAGE_envoyer.svg'
+import C4Corbeille2 from './img/MESSAGE_favoris.svg'
+import C4Corbeille3 from './img/MESSAGE_poubelle.svg'
+
+import C5Etiquette1 from './img/VESTON_logoA.svg'
+import C5Etiquette2 from './img/VESTON_logoB.svg'
+import C5Etiquette3 from './img/VESTON_logoC.svg'
+
 import video01 from './videos/01_BonneAnnee.mp4'
 
 export {
@@ -22,5 +42,25 @@ export {
     imgPage,
     imgPhoto,
     testSVG,
-    video01
+    video01,
+
+    C1Heures1,
+    C1Heures2,
+    C1Heures3,
+
+    C2Menace1,
+    C2Menace2,
+    C2Menace3,
+
+    C3Code1,
+    C3Code2,
+    C3Code3,
+
+    C4Corbeille1,
+    C4Corbeille2,
+    C4Corbeille3,
+
+    C5Etiquette1,
+    C5Etiquette2,
+    C5Etiquette3,
 }
